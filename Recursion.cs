88e5818273e33b30e0fcb0f234873628cfe353d2
Ravi Raj <meraviraj05@gmail.com>
2024-06-19 00:43:08 +0530

    Recursion and  Itrative Difference

---
+++ Recursion.cs
@@ -0,0 +1,31 @@
+﻿class Recursion
+{
+    public void CalculateItrative(int N)
+    {
+        while(N>0)
+        {
+            int K = N * N;
+            Console.WriteLine(K); // Time Complaxity : (n+1):=> O(n): Linear 
+           N= N - 1;
+
+        }
+    }
+    static void Main(string[] args)
+    {
+      Recursion r=new Recursion();
+        //r.CalculateItrative(10);
+        r.CalculateRecursive(10);
+        Console.ReadKey();
+    }
+
+    public void CalculateRecursive(int N)
+    {
+        if (N > 0)
+        {
+            int K = N * N;
+            Console.WriteLine(K);
+            CalculateRecursive(N-1); 
+
+        }
+    }
+}
