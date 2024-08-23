9b51e5682c5a4c8a882c2a857fd7377810ecfbc1
Ravi Raj <meraviraj05@gmail.com>
2024-08-23 20:48:22 +0530

    Binary Tree Creation

---
+++ BinarySearch Representaion Link/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace BinarySearch_Representaion_Link
+{
+
+    public class Node
+    {
+        public int val { get; set; }
+        public Node? leftPointer { get; set; }
+        public Node? rightPointer { get; set; }
+        public Node(int val)
+        {
+
+            this.val = val;
+            leftPointer = null;
+            rightPointer = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node? Root { get; set; }
+        public BinaryTree(int rootval)
+        {
+            // Setting Root Value:
+            Root = new Node(rootval);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+
+            // Defining Root value:
+            BinaryTree tree = new BinaryTree(12);
+            tree.Root.leftPointer= new Node(1);
+            tree.Root.rightPointer= new Node(2);
+            tree.Root.leftPointer.leftPointer=new Node(3);
+            tree.Root.leftPointer.rightPointer=new Node(4);
+            tree.Root.rightPointer.leftPointer= new Node(5);
+            tree.Root.rightPointer.rightPointer= new Node(6);
+
+            Console.WriteLine("Binary Tree "+ tree.Root.val);
+           
+        }
+    }
+}
---
+++ Double Ended Queue/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Double_Ended_Queue
+{
+    public class DoubleEndedQ
+    {
+        private int[] arr;
+        private int max;
+        private int front;
+        private int back;
+        int counter;
+       
+
+        public DoubleEndedQ(int size)
+        {
+            this.max = size;
+            this.arr = new int[size];
+            this.front = -1;
+            this.back = 0;
+            this.counter = 0;
+         
+        }
+
+        public bool isEmpty()
+        {
+            return counter == 0;
+        }
+        public bool isFull()
+        {
+            return  counter == max;
+        }
+
+        public void enqueueFront(int val)
+        {
+            if (isFull()) { Console.WriteLine("Queue is full"); return; }
+
+            if (counterf <= max&&counterb==0)
+            {
+                front--;
+                counterf--;
+                arr[++front] = val;
+
+            }
+          
+        }
+        public void enqueueBack(int val)
+        {
+            if (isFull()) { Console.WriteLine("Queue is full"); return; }
+
+            if (isEmpty())
+            {
+                arr[++back] = val;
+                counterb++;
+            }
+            else
+            {
+                back--;
+                counterb--;
+                arr[++back] = val;
+                counterb++;
+            }
+
+
+        }
+
+        public void dequeueFront()
+        {
+            if (counterf >= 0) {
+                Console.WriteLine("dequefront: " + arr[front]);
+                front++;
+                counterf++;
+            }      
+            
+        }
+        public void Print()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Empty");
+                return;
+            }
+            for (int i = front; i <= back; i++)
+            {
+                Console.Write(" Element: " + arr[i]);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleEndedQ queue = new DoubleEndedQ(4);
+            queue.enqueueFront(1);
+            queue.enqueueBack(2);
+            queue.enqueueBack(4);
+            queue.Print();
+        }
+    }
+}
